08424b2e0f8fd4bf821a00c0431f3981dfe40a5f
gauravav <avulagaurav@gmail.com>
2023-03-20 16:29:32 -0400

    Database script for players table and inserting values in players table

--- Scripts.sql
+++ Scripts.sql
@@ -1,4 +1,5 @@
 CREATE DATABASE IF NOT EXISTS TEAMPROJECT;
+DROP TABLE IF EXISTS players;
 DROP TABLE IF EXISTS teams;
 CREATE TABLE teams (
                        id INT AUTO_INCREMENT PRIMARY KEY,
@@ -6,6 +7,12 @@ CREATE TABLE teams (
                        time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-
+CREATE TABLE players (
+                         id INT AUTO_INCREMENT PRIMARY KEY,
+                         name VARCHAR(255) NOT NULL,
+                         team_id INT NOT NULL,
+                         FOREIGN KEY (team_id) REFERENCES teams(id)
+);
 insert into teams (name) values ('Arsenal');
 insert into teams (name) values ('Real Madrid');
+insert into players (name, team_id) values ('Cristiano Ronaldo', 1);
